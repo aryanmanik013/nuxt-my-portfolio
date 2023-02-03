a31d8236a00723205f2e211418d0e42c2a9189ad
aryanmanik013 <aryanmanik440@gmail.com>
2023-02-03 17:40:58 +0530

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark" class="changeNav">
+      <b-navbar-nav>
+         <b-nav-item>
+            <NuxtLink to="/">Home</NuxtLink>
+        </b-nav-item>
+        <b-nav-item>
+            <NuxtLink to="/">Work</NuxtLink>
+        </b-nav-item>
+
+        <b-nav-item>
+            <NuxtLink to="/aboutus">About Us</NuxtLink>
+
+        </b-nav-item>
+        <b-nav-item>
+            <NuxtLink to="/">Contact Us</NuxtLink>
+
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <div class="ml-auto">
+        <b-navbar-nav>
+          <b-nav-item>Contact</b-nav-item>
+        </b-navbar-nav>
+      </div>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
+<style scoped>
+/deep/.nav-link a{
+  color: white !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="container-fluid main-Div">
+    <div class="row">
+      <div class="col-md-12">
+        <Navbar />
+        <div class="d-flex justify-content-center">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "../static/style.css";
+export default {};
+</script>
+
+<style>
+
+</style>
